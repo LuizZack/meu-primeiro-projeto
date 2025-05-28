5bf4a0c59c23383127761a050f9bc30425738504
HeegllenC <hergllen@outlook.com>
2025-05-27 21:11:46 -0300

    zackproj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NomeComponenteComponent } from './nome-componente/nome-componente.component';
+import { PerfilComponent } from "./perfil/perfil.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NomeComponenteComponent],
-  standalone:true
+  imports: [RouterOutlet, PerfilComponent],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [FormsModule],
   standalone:true,
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
 export class PerfilComponent {
-
-  username:'fulanosilva' | undefined,
-  nome: 'Fulano da Silva',
-  local: 'Manaus',
-
-
-
-
-
-
+  nome  = '';
+  perfil = {
+    username: "LuizZack",
+    nome: "Luiz Zack Ferreira melo",
+    local: "Manaus",
+    idade: 19,
+    profissao: "Desenvolvedor Web",
+    empresa: "FpfTech",
+    publicacoes: 105,
+    seguidores: 1133,
+    seguindo: 150,
+  foto: 'https://img.freepik.com/vetores-premium/projeto-de-ilustracao-vetorial-face-avater-de-jovem_968209-13.jpg'
+  };
 }
